8b2d9836b30558fe38d7e81a49288b8d1fafb492
Tianshuai Yu <yglacier@gmail.com>
2018-11-10 15:16:11 +0900

    Unity Project

---
+++ Calculator/Assets/Scripts/SportsListController.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SportsListController : MonoBehaviour {
+
+    //[SerializeField]
+    Sports[] list;
+
+    public void SetList(int index, Sports sport)
+    {
+        list[index] = sport;
+        UpdateResult();
+    }
+
+    private void Start()
+    {
+        list = new Sports[1];
+    }
+
+    void UpdateResult()
+    {
+        float result = 0f;
+        foreach (Sports sport in list) {
+            result += (sport.Ticket + sport.Live - sport.Cost);
+        }
+        GetComponent<Text>().text = result.ToString();
+    }
+}
